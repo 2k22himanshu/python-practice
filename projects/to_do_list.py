a040a90704fc69c8dcb40c39e6f80eb15841d36b
himanshu-ai <2k22.aiml.2212039@gmail.com>
2026-05-26 18:23:20 +0530

    Create to_do_list.py

---
+++ projects/to_do_list.py
@@ -0,0 +1,27 @@
+# Simple To-Do List
+
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task Added")
+
+    elif choice == "2":
+        print("\nTasks:")
+        for task in tasks:
+            print("-", task)
+
+    elif choice == "3":
+        print("Exiting Program")
+        break
+
+    else:
+        print("Invalid Choice")
