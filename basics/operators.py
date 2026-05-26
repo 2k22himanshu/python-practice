035810a16737379f64e1be510f885064c0fc53c5
himanshu-ai <2k22.aiml.2212039@gmail.com>
2026-05-26 16:11:36 +0530

    Create operators.py

---
+++ basics/operators.py
@@ -0,0 +1,29 @@
+# Operators in Python
+
+# Arithmetic Operators
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+
+# Comparison Operators
+print("Is a greater than b?", a > b)
+print("Is a equal to b?", a == b)
+
+# Logical Operators
+x = True
+y = False
+
+print("AND Operator:", x and y)
+print("OR Operator:", x or y)
+print("NOT Operator:", not x)
+
+# Assignment Operator
+c = 20
+c += 5
+
+print("Updated Value of c:", c)
