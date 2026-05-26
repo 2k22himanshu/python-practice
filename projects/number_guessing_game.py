55a4723d27c5890f2414ded458e7d29ebc91d151
himanshu-ai <2k22.aiml.2212039@gmail.com>
2026-05-26 18:21:26 +0530

    Create number_guessing_game.py

---
+++ projects/number_guessing_game.py
@@ -0,0 +1,11 @@
+# Number Guessing Game
+
+secret_number = 7
+
+guess = int(input("Guess the number: "))
+
+if guess == secret_number:
+    print("Congratulations! You guessed correctly.")
+
+else:
+    print("Wrong guess")
